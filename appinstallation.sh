648fa428edd06a0390381b1a81cdbd07a8fdd28b
Siva <rsk.sfdctraining1@gmail.com>
2024-04-16 21:24:48 +0530

    varibale file commit

--- appinstallation.sh
+++ appinstallation.sh
@@ -5,14 +5,15 @@ userid=$(id -u)
 if [ $userid -ne 0 ]
 then
     echo "This user is not a sudo user. Please get the sudo access"
+    exit 1
 fi
 
-dnf install mysql-server -y
+dnf install mysql -y
 
 if [ $? -ne 0 ]
 then
     echo "This my sql server installation is not completed successfully"
-
+    exit 1
 fi
 
 echo "Still continuing the execution"
