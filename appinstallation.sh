cf9231911919c91b27d7728826475ad710a9122c
Siva <rsk.sfdctraining1@gmail.com>
2024-04-16 21:18:26 +0530

    varibale file commit

---
+++ appinstallation.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+userid=${id-u}
+
+if [$userid -ne 0]
+then
+    echo "This user is not a sudo user. Please get the sudo access"
+fi
+
+dnf install mysql-server -y
+
+echo "Still continuing the execution"
