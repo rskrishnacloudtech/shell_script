2af9b29c6378b45da4bb0a95ef9cc403918bdfc0
Siva <rsk.sfdctraining1@gmail.com>
2024-04-26 12:48:35 +0530

    calling script from antoher script

--- script2.sh
+++ script2.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-scrip2var="This is the variable from script11"
-pid=$(echo $$)
+scrip2var="This is the variable from script2"
 
 echo "This is the varibale from script 2: $scrip2var"
-echo "This the pid of the sript 2: $pid"
+echo "This the pid of the sript 2: $$"
 
 #calling script1 into script2
 ./script1.sh
 
 echo "This is the varaible value of script1: $scrip1var"
-echo "This the pid of the sript 1: $pid"
+echo "This the pid of the sript 1: $$"
