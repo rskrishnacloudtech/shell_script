d8d99da62382b447379943f1d917dca11c49b5cd
Siva <rsk.sfdctraining1@gmail.com>
2024-04-26 12:50:05 +0530

    calling script from antoher script

--- script1.sh
+++ script1.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-scrip1var="This is the variable from script11"
-pid=$(echo $$)
+VAR="This is the variable from script11"
+#pid=$(echo $$)
 
-echo "This is the variable from currnet script: $scrip1var"
-echo "PID of the script 1 is: $pid"
+echo "This is the variable from currnet script: $VAR"
+echo "PID of the script 1 is: $$"
 
 
--- script2.sh
+++ script2.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-scrip2var="This is the variable from script2"
+VAR="This is the variable from script2"
 
-echo "This is the varibale from script 2: $scrip2var"
+echo "This is the varibale from script 2: $VAR"
 echo "This the pid of the sript 2: $$"
 
 #calling script1 into script2
 ./script1.sh
 
-echo "This is the varaible value of script1: $scrip1var"
+echo "This is the varaible value of script1: $VAR"
 echo "This the pid of the sript 1: $$"
