792f8c7ecf0db9c4f67eba39994b84b09ff26a85
Siva <rsk.sfdctraining1@gmail.com>
2024-04-26 13:09:58 +0530

    calling script from antoher script

--- script2.sh
+++ script2.sh
@@ -5,8 +5,11 @@ VAR="This is the variable from script2"
 echo "Before calling other script: $VAR"
 echo "This the pid of the sript 2: $$"
 
-#calling script1 into script2
-./script1.sh
+#calling script1 into script2 in WAY1.
+#./script1.sh
+
+#calling script1 into script2 in WAY1.
+source ./script1.sh
 
 echo "After calling other script: $VAR" 
 echo "This the pid of the sript1: $$"
